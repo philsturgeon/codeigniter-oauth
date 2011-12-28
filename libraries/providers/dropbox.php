3d624bf3e1d7f6b2a99069b86981d1fdf27752a9
Phil Sturgeon <email@philsturgeon.co.uk>
2011-12-28 18:50:22 +0000

    Updated to v1 of the Dropbox API.

--- libraries/providers/dropbox.php
+++ libraries/providers/dropbox.php
@@ -6,23 +6,23 @@ class OAuth_Provider_Dropbox extends OAuth_Provider {
 
 	public function url_request_token()
 	{
-		return 'https://api.dropbox.com/0/oauth/request_token';
+		return 'https://api.dropbox.com/1/oauth/request_token';
 	}
 
 	public function url_authorize()
 	{
-		return 'http://www.dropbox.com/0/oauth/authorize';
+		return 'http://www.dropbox.com/1/oauth/authorize';
 	}
 
 	public function url_access_token()
 	{
-		return 'https://api.dropbox.com/0/oauth/access_token';
+		return 'https://api.dropbox.com/1/oauth/access_token';
 	}
 	
 	public function get_user_info(OAuth_Consumer $consumer, OAuth_Token $token)
 	{
 		// Create a new GET request with the required parameters
-		$request = OAuth_Request::forge('resource', 'GET', 'https://api.dropbox.com/0/account/info', array(
+		$request = OAuth_Request::forge('resource', 'GET', 'https://api.dropbox.com/1/account/info', array(
 			'oauth_consumer_key' => $consumer->key,
 			'oauth_token' => $token->access_token,
 		));
@@ -36,6 +36,7 @@ class OAuth_Provider_Dropbox extends OAuth_Provider {
 		return array(
 			'uid' => $token->uid,
 			'name' => $user->display_name,
+			'email' => $user->email,
 			'location' => $user->country,
 		);
 	}
